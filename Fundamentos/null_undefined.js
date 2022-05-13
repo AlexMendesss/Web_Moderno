3e20fa473644570d0dfb48c3e191f44ba06d91b3
AlexMendesss <alex.silvacarvalho015@gmail.com>
2022-05-12 21:24:32 -0300

    Atualização Estudos

---
+++ Fundamentos/null_undefined.js
@@ -0,0 +1,11 @@
+let valor; //não inicializa a variável
+
+console.log(valor);
+
+valor = null; //Variável inicializada, mas, sem valor
+
+console.log(valor);
+
+valor = 10; //Variável inicializada, e com valor definido
+
+console.log(valor);
